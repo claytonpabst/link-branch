2c6a2dc5607e8ddff665482672fcaffbbf969536
Clayton Pabst <claytonpabst@gmail.com>
2018-09-20 03:34:39 -0600

    fixed header style

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import {Link} from 'react-router-dom';
 
 import './MainHeader.css'
 
@@ -25,7 +26,7 @@ class MainHeader extends Component {
           {(global) => (
             <div>
               <div style={{float:'left'}}>
-                <h1 className="main-header_main-logo">LB</h1>
+                <Link to="/" style={{textDecoration:"none"}}><h1 className="main-header_main-logo">LB</h1></Link>
               </div>
               <div style={{float:"left"}}>
                 <button className="main-header_sign-in-button" onClick={global.state.authenticated ? global.toggleSignOutModel : global.toggleSignInModel}>{global.state.authenticated ? this.buildUsername(global.state.username) : "Sign In/Sign Up"}</button>
